a92f2f19a61b8a44765d961e4d5598085b14aca8
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-01-17 17:57:24 +0530

    Update fixes1.sh

--- fixes1.sh
+++ fixes1.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-
-
-
-
-
-cd frameworks/base/
-git fetch https://github.com/xc112lg/android_frameworks_base-1.git patch-3
-git cherry-pick 61723d6220ad417008c50565bc9212e9cec96450
-cd ../../
-
 
