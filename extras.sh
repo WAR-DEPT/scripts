23cd172fc08be9b15c751402175493de3823f4dd
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-01-17 17:54:53 +0530

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -3,18 +3,8 @@
 # S_asb_2023-03
 # repopick -t S_asb_2023-03
 
-# Charging animation
-cd frameworks/base/
-git cherry-pick 10a30bf4bbee8e98a742338ef89f0a414ff638b9
-cd ../../
-
 # PixelProps
 cd frameworks/base/
-git cherry-pick 9c542fb2323aae953d9957d320ba8ae1603aaafe
+git cherry-pick 0dd44af1acfc6f29bb62bb6d6d094f890684cc3c 43db68bf7b9a140c5e86dc76043b98caa3e562ce
 cd ../../
 
-# Updater
-cd vendor/lineage
-git fetch https://github.com/LG-G6/android_vendor_lineage.git lineage-19.1
-git cherry-pick 8246f8702827e7ca209e607dcc17cf4f1dba998f
-cd ../../
