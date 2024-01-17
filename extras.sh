da110c6ecbb12d51d7eb29b7bc191ad672de3f08
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-01-17 17:55:56 +0530

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -4,8 +4,4 @@
 # repopick -t S_asb_2023-03
 
 # PixelProps
-cd frameworks/base/
-git fetch https://github.com/LG-G6/android_frameworks_base.git lineage-20.0
-git cherry-pick 0dd44af1acfc6f29bb62bb6d6d094f890684cc3c 43db68bf7b9a140c5e86dc76043b98caa3e562ce
-cd ../../
 
