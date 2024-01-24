e2aead8c59d0839b27fbd6a464de51411bf1b4ae
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-01-24 16:24:35 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 
-
-rm -rf vendor/lineage/
-rm -rf vendor/derp
-rm -rf build/make/
-
 rm out/target/product/*/*.zip
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 rm -rf external/libcxx
