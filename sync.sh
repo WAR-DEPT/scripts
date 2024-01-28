75c8ab175662b72d4902b9913aaa8c4fd7c69ecc
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-01-28 15:41:34 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+rm -rf kenrel/xiaomi/sm6250
+rm -rf device/xiaomi/sm6250-common
+rm -rf device/xiaomi/miatoll
+rm -rf vendor/xiaomi/sm6250-common
+rm -rf vendor/xiaomi/miatoll
+
+
 
 rm out/target/product/*/*.zip
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
