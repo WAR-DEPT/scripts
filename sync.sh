fdf99fc51a5b891badd8a768405bff6f7bd33fd5
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-02-10 20:57:43 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf vendor/gapps/overlay/GmsSettingsProviderOverlay
+rm -rf vendor/gapps
 
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
