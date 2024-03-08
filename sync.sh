43bbc527ece8b1572d9887cb262d6ace87e0db66
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-03-08 20:18:07 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-rm -rf kernel/xiaomi/miatoll
 
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 
 
-lunch lineage_miatoll-userdebug
-m bacon
+lunch aosp_miatoll-userdebug
+mka bacon
