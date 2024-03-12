988c465405a10ba657c0b8bc40cb6771d4dd3272
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-03-12 11:18:35 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-
+rm prebuilts 
 
 rm out/target/product/*/*.zip
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 
 source scripts/fixes.sh
-#lunch derp_h870-userdebug
-#mka derp
-#lunch derp_us997-userdebug
-#mka derp
+
 lunch lineage_miatoll-userdebug
 m bacon 
 
