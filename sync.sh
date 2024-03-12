19cf1f88559324a4da6645b6680bd19e7f64d77b
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-03-12 16:12:50 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -7,7 +7,6 @@ rm out/target/product/*/*.zip
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 
-source scripts/fixes.sh
 
 lunch lineage_miatoll-userdebug
 m bacon 
